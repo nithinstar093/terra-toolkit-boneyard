f36e4461cc72e0a65cfc3e95e0086781bf2cb44a
Sireesha Nekkanti <nekkanti.sirisha1@gmail.com>
2020-04-26 23:54:42 -0600

    formatted result.json file

--- src/wdio/WdioFileReporter.js
+++ src/wdio/WdioFileReporter.js
@@ -1,8 +1,3 @@
-/* eslint-disable prefer-rest-params */
-/* eslint-disable no-continue */
-/* eslint-disable no-restricted-syntax */
-/* eslint-disable guard-for-in */
-/* eslint-disable class-methods-use-this */
 const WDIOSpecReporter = require('wdio-spec-reporter/build/reporter');
 const jsonMultilineStrings = require('json-multiline-strings');
 const stripAnsi = require('strip-ansi');
@@ -10,74 +5,19 @@ const fs = require('fs');
 const path = require('path');
 
 const jsonFilePath = path.resolve(__dirname, './result.json');
-
+const resultData = {};
 class WdioCustomeReporter extends WDIOSpecReporter {
   constructor(globalConfig, options) {
     super(globalConfig);
     this.isSpecStarted = false;
     this.options = options;
-    this.getSuiteCustomResult = this.getSuiteCustomResult.bind(this);
     this.runners = [];
     this.on('runner:end', (runner) => {
-      console.log('runner inside cons', runner);
       this.runners.push(runner);
     });
   }
 
-  getSuiteCustomResult(runner) {
-    const { cid } = runner;
-    const { stats } = this.baseReporter;
-
-    const results = stats.runners[cid];
-    const preface = `[${this.getBrowserCombo(results.capabilities, false)} #${cid}]`;
-    const specHash = stats.getSpecHash(runner);
-    const spec = results.specs[specHash];
-    const combo = this.getBrowserCombo(results.capabilities);
-    const failures = stats.getFailures().filter((f) => f.cid === cid || Object.keys(f.runner).indexOf(cid) > -1);
-
-    /**
-     * don't print anything if no specs where executed
-     */
-    if (Object.keys(spec.suites).length === 0) {
-      return '';
-    }
-
-    this.errorCount = 0;
-    let output = '';
-
-    output += '------------------------------------------------------------------\n';
-
-    /**
-     * won't be available when running multiremote tests
-     */
-    if (results.sessionID) {
-      output += `${preface} Session ID: ${results.sessionID}\n`;
-    }
-
-    output += `${preface} Spec: ${this.specs[cid]}\n`;
-
-    /**
-     * won't be available when running multiremote tests
-     */
-    if (combo) {
-      output += `${preface} Running: ${combo}\n`;
-    }
-
-    output += `${preface}\n`;
-    output += this.getResultList(cid, spec.suites, preface);
-    output += `${preface}\n`;
-    // eslint-disable-next-line no-underscore-dangle
-    output += this.getSummary(this.results[cid], spec._duration, preface);
-    output += this.getFailureList(failures, preface);
-    output += this.getJobLink(results, preface);
-    output += `${preface}\n`;
-    return output;
-  }
-
   printSuitesSummary() {
-    const resultData = {
-      result: [],
-    };
     const { runners } = this;
     if (runners && runners.length) {
       runners.forEach((runner) => {
@@ -89,12 +29,15 @@ class WdioCustomeReporter extends WDIOSpecReporter {
           theme: process.env.THEME || 'default-theme',
           locale: process.env.LOCALE,
           formFactor: process.env.FORM_FACTOR,
-          result: stripAnsi(this.getSuiteCustomResult(runner)),
+          result: stripAnsi(this.getSuiteResult(runner)),
           endDate,
         };
-        resultData.result.push(jsonObject);
+        if (!resultData[runner.specHash]) {
+          resultData[runner.specHash] = [];
+        }
+        resultData[runner.specHash].push(jsonObject);
       });
-      fs.appendFileSync(jsonFilePath, `${JSON.stringify(jsonMultilineStrings.split(resultData), null, 2)}\n\n`, (err) => {
+      fs.writeFileSync(jsonFilePath, `${JSON.stringify(jsonMultilineStrings.split(resultData), null, 2)}\n\n`, (err) => {
         if (err) {
           console.log(`File Error -> ${err.message}`);
         }
