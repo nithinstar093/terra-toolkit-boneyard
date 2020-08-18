521730c3d049dd22eefbbeff59aecd1f4a64c303
Supreeth MR <supreeth.mr1990@gmail.com>
2020-08-18 23:37:25 +0530

    fixed config error

--- scripts/lighthouse/lightHouseConfig.js
+++ scripts/lighthouse/lightHouseConfig.js
@@ -2,7 +2,7 @@
 // Corresponds to "Dense 4G 25th percentile" in https://docs.google.com/document/d/1Ft1Bnq9-t4jK5egLSOc28IL4TvR-Tt0se_1faTA4KTY/edit#heading=h.bb7nfy2x9e5v
 const { desktopDense4G } = require('lighthouse/lighthouse-core/config/constants.js').throttling;
 
-const lighthouseConfig = (isMobileDevice) => ({
+const lightHouseConfig = (isMobileDevice) => ({
   extends: 'lighthouse:default',
   settings: {
     onlyCategories: ['performance'],
@@ -15,4 +15,4 @@ const lighthouseConfig = (isMobileDevice) => ({
   },
 });
 
-module.exports = lighthouseConfig;
+module.exports = lightHouseConfig;
--- src/wdio/services/LightHouseService.js
+++ src/wdio/services/LightHouseService.js
@@ -4,29 +4,25 @@ const { addReportData, generateReport } = require('../../../scripts/lighthouse/r
 const { terraViewports } = require('../../../config/wdio/services.default-config');
 
 const rootDir = 'performance_reports';
-const htmlRootDir = 'performance_reports/html';
-let viewPortSize;
+const htmlRootDir = 'performance_reports//html';
 
 export default class LightHouseService {
   before() {
     if (fs.existsSync(rootDir)) {
-      const files = fs.readdirSync(htmlRootDir)
-      if (files.length > 0) {
-        files.forEach((filename) => {
-          fs.unlinkSync(`${htmlRootDir}//${filename}`);
-        });
-      }
+      const files = fs.readdirSync(htmlRootDir);
+      files.forEach((filename) => {
+        fs.unlinkSync(`${htmlRootDir}//${filename}`);
+      });
       fs.rmdirSync(htmlRootDir);
       fs.rmdirSync(rootDir);
     }
-    viewPortSize =  global.browser.getViewportSize().width;
   }
 
   async afterTest(test) {
     const url = await global.browser.getUrl();
-    const isMobileDevice = (viewPortSize <= terraViewports.small.width);
-    let fileName = (isMobileDevice) ? test.fullTitle.replace(/tiny|small/gi, 'Mobile') : test.fullTitle.replace(/medium|large|huge|enormous/gi, 'Desktop');
-    fileName = fileName.replace('-is-accessible-and-is-within-the-mismatch-tolerance', '').trim();
+    const isMobileDevice = test.fullTitle.includes('tiny') || test.fullTitle.includes('small');
+    let fileName = test.fullTitle.replace('-is-accessible-and-is-within-the-mismatch-tolerance', '').trim();
+    fileName = (isMobileDevice) ? fileName.replace(/tiny|small/gi, 'Mobile') : fileName.replace(/medium|large|huge|enormous/gi, 'Desktop');
     const htmlFileUrl = `${fileName.replace(/ /g, '_')}.html`;
 
     if (!fs.existsSync(rootDir)) fs.mkdirSync(rootDir);
